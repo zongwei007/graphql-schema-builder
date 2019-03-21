93b8d1030e256b64c295fdde548b939e411f407f
Zongwei <zongwei007@gmail.com>
2019-03-21 22:38:44 +0800

    test: 添加 Common 属性测试

--- src/test/java/com/ltsoft/graphql/example/ObjectWithExtension.java
+++ src/test/java/com/ltsoft/graphql/example/ObjectWithExtension.java
@@ -1,9 +1,11 @@
 package com.ltsoft.graphql.example;
 
+import com.ltsoft.graphql.annotations.GraphQLComment;
 import com.ltsoft.graphql.annotations.GraphQLFieldExtension;
 import com.ltsoft.graphql.annotations.GraphQLType;
 
 @GraphQLType
 @GraphQLFieldExtension(ObjectDefineExtension.class)
+@GraphQLComment("It is a comment")
 public class ObjectWithExtension {
 }
